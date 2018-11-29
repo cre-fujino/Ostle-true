8598be53c6fc72525af69d7afe6504a31eed10cb
cre-fujino <fujino@hokkaido-ima.co.jp>
2018-11-29 23:38:35 +0900

    トップ

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Game Start
           </a>
         </header>
       </div>
