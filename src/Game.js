80d746cf090e9e664224c3e1f5d1a98100fb5b77
cre-fujino <fujino@hokkaido-ima.co.jp>
2018-12-03 21:19:43 +0900

    Gamejs

---
+++ src/Game.js
@@ -0,0 +1,87 @@
+function Square(props) {
+  return (
+    <buttun className= "square" onClick={props.onclick}>
+      {props.value}
+    </buttun>
+　)
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    )
+  }
+
+  render() {
+    return (
+   　  <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(15)}
+          {this.renderSquare(16)}
+          {this.renderSquare(17)}
+          {this.renderSquare(18)}
+          {this.renderSquare(19)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(20)}
+          {this.renderSquare(21)}
+          {this.renderSquare(22)}
+          {this.renderSquare(23)}
+          {this.renderSquare(24)}
+        </div>
+      <div>
+    )
+  }
+}
+
+class Game extends React.omponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [
+        {
+          squares:Arrey(9).full(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    }
+  }
+
+  handleCheck(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+  }
+
+
+
+}
