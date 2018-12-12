d955d544029b1b37ba46f4d3598836a00c024cdc
cre-fujino <fujino@hokkaido-ima.co.jp>
2018-12-12 23:08:24 +0900

    Game

--- src/Game.js
+++ src/Game.js
@@ -3,67 +3,87 @@ import App from './App.js'
 import './Game.css'
 import  { BrowserRouter, Route, Link } from 'react-router-dom'
 
-function Square(props) {
+class Square extends React.Component {
+  render() {
     return (
-    <buttun className= "square" onClick={props.onclick}>
-      {props.value}
-    </buttun>
-  )
+      <buttun className= "square">
+        {this.props.value}
+      </buttun>
+    )
+  }
 }
 
-class Game extends React.Component {
+class Board extends React.Component {
   renderSquare(i) {
-    return (
-      <Square value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    )
+    return <Square value= {i} />
   }
 
   render() {
-    return (
-        <BrowserRouter>
-        <div>
+    cosnt status = 'Next player: X'
 
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
+    return (
+        <div>
+          <div className="status">{status}</div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(5)}
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+              {this.renderSquare(9)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(10)}
+              {this.renderSquare(11)}
+              {this.renderSquare(12)}
+              {this.renderSquare(13)}
+              {this.renderSquare(14)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(15)}
+              {this.renderSquare(16)}
+              {this.renderSquare(17)}
+              {this.renderSquare(18)}
+              {this.renderSquare(19)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(20)}
+              {this.renderSquare(21)}
+              {this.renderSquare(22)}
+              {this.renderSquare(23)}
+              {this.renderSquare(24)}
+            </div>
+          </div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(5)}
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-          {this.renderSquare(9)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(10)}
-          {this.renderSquare(11)}
-          {this.renderSquare(12)}
-          {this.renderSquare(13)}
-          {this.renderSquare(14)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(15)}
-          {this.renderSquare(16)}
-          {this.renderSquare(17)}
-          {this.renderSquare(18)}
-          {this.renderSquare(19)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(20)}
-          {this.renderSquare(21)}
-          {this.renderSquare(22)}
-          {this.renderSquare(23)}
-          {this.renderSquare(24)}
-        </div> */}
-      </div>
-   </BrowserRouter>
-   )
+    )
   }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+
+
+
+
+
+
 }
 
 
